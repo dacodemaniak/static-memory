3f5634c38985b29f6ee6e148ad89e7d383ddf642
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2020-06-11 12:06:30 +0200

    Playing engine implementation

--- src/card-event-manager.js
+++ src/card-event-manager.js
@@ -9,6 +9,7 @@
 export default class CardEventManager {
     constructor() {
         this._click() // Invoke click handler on cards
+        this._playingCard = null; // Map for played cards
     }
 
     _click() {
@@ -18,23 +19,70 @@ export default class CardEventManager {
                 const element = $(event.target) // Cible de l'événement...
                 if (element.hasClass('hidden-face')) {
                     console.log(`Reveal ${element.attr('data-rel')}`)
-                    element
-                        .addClass('flip-out')
-                    setTimeout(
-                        () => {
-                            element
-                                .removeClass('hidden-face')
-                                .removeClass('flip-out')
-                                .addClass('flip-in')
-                        },
-                        500
-                    )
+                    this._addCard(element)
                 } else {
-                    element
-                        .addClass('hidden-face')
-                        .removeClass('flip-in')
+                    this._removeCard(element)
                 }
             }
         )
+    }
+
+    _addCard(element) {
+        // Reveal the element clicked
+        element
+            .addClass('flip-out')
+        
+        setTimeout(
+            () => {
+                element
+                    .removeClass('hidden-face')
+                    .removeClass('flip-out')
+                    .addClass('flip-in')
+            },
+            500
+        )
+        if (this._playingCard !== null) {
+            console.log('A card was previously played')
+            // Some cards was played, try to know if both are same
+            if (element.attr('data-rel') === this._playingCard.attr('data-rel')) {
+                // Pair was found... So freeze cards
+                element
+                    .addClass('freezed-card')
+                    .removeClass('card')
+                this._playingCard
+                    .addClass('freezed-card')
+                    .removeClass('card')
+                // Sets played card as null, to make another pick
+                this._playingCard = null;
+            } else {
+                // Turn off cards after delay
+                setTimeout(
+                    () => {
+                        console.log(`Hide current card : ${element.attr('data-rel')}`)
+                        element
+                            .removeClass('flip-in')
+                            .addClass('hidden-face')
+                        console.log(` Hide played card : ${this._playingCard.attr('data-rel')}`)
+                        this._playingCard
+                            .removeClass('flip-in')
+                            .addClass('hidden-face')
+                        // Don't forget to empty the played card
+                        this._playingCard = null;
+                    },
+                    1500
+                )
+            }
+        } else {
+            // Store clicked card... and wait for next card
+            console.log(`Store ${element.attr('data-rel')} as played card`)
+            this._playingCard = element;
+        }
+    }
+
+    _removeCard(element) {
+        element
+            .addClass('hidden-face')
+            .removeClass('flip-in')
+        this._playingCard = null;
     }
 }
