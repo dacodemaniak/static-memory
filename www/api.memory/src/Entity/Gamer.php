d8e31ccbbde9aa7cfa16049648883eb5982d354a
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2020-06-17 08:51:33 +0200

    Add gamer entity

---
+++ www/api.memory/src/Entity/Gamer.php
@@ -0,0 +1,66 @@
+<?php
+namespace Memory\Entity;
+
+/**
+ * @name Gamer
+ * @author IDea Factory (jean-luc.a@ideafactory.fr) - June 2020
+ * @version 1.0.0
+ * @namespace Memory\Entity
+ * @category Entity
+ * @abstract Poor entity implementation (without Doctrine support!)
+ */
+class Gamer {
+
+    /**
+     * ID for the gamer
+     * @var int
+     */
+    private $id;
+    
+    /**
+     * Name of the gamer
+     * @var string
+     */
+    private $name;
+    
+    public function __construct(){}
+    
+    /**
+     * Get the ID of the gamer
+     * @return int
+     */
+    public function getId(): ?int {
+        return $this->id;
+    }
+    
+    /**
+     * Sets the ID of the gamer
+     * @param int $id
+     * @return Gamer
+     */
+    public function setId(int $id): Gamer {
+        $this->id = $id;
+        
+        return $this;
+    }
+    
+    /**
+     * Sets the gamer name
+     * @param string $name
+     * @return Gamer
+     */
+    public function setName(string $name): Gamer {
+        $this->name = $name;
+        
+        return $this;
+    }
+    
+    /**
+     * Gets the gamer name
+     * @return string
+     */
+    public function getName(): ?string {
+        return $this->name;
+    }
+}
+
