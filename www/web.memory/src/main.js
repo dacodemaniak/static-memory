0449e8e16fa697486d549e467de68b954208c25f
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-05-31 19:41:11 +0200

    Module replacement Load config both dev or prod according build cli

--- www/web.memory/src/main.js
+++ www/web.memory/src/main.js
@@ -6,6 +6,7 @@
 import $ from 'jquery'
 import * as materialize from 'materialize-css'
 import Platform from './platform'
+import config from './config-APP_TARGET';
 
 // Load SCSS at transpile time
 import css from './scss/main.scss'
@@ -13,6 +14,7 @@ import BestPlayers from './best-players'
 
 class Main {
     constructor() {
+        console.log(config.devMode)
         this.gamePlatform = new Platform()
     }
 
--- www/web.memory/webpack.config.js
+++ www/web.memory/webpack.config.js
@@ -2,43 +2,51 @@ const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        chunkFilename: 'vendor.js'
-    },
-    // Babel to translate es6 to old school es5
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: '/node_modules',
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.sc|ass$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'sass-loader'
+module.exports = function(env) {
+    // Read --env args from webpack build, default to dev
+    let appTarget = env.APP_TARGET || 'dev';
+
+    return {
+        entry: './src/main.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'main.js',
+            chunkFilename: 'vendor.js'
+        },
+        // Babel to translate es6 to old school es5
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: '/node_modules',
+                    use: {
+                        loader: 'babel-loader'
                     }
-                ]
-            }
+                },
+                {
+                    test: /\.sc|ass$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: 'css-loader'
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: './../assets/css/[name].css',
+                chunkFileName: '../../assets/css/[id].css'
+            }),
+            new webpack.NormalModuleReplacementPlugin(/(.*)-APP_TARGET(\.*)/, function(resource) {
+                resource.request = resource.request.replace(/-APP_TARGET/, `-${appTarget}`);
+            })
         ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: './../assets/css/[name].css',
-            chunkFileName: '../../assets/css/[id].css'
-        })
-    ]
+    }
 }
